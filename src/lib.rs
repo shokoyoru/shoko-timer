4263559c0e11a3dbb24e8aa49db66e8cd061bc3f
shokomergo <shokomergo@gmail.com>
2026-02-26 10:32:46 +0300

    refactor: move core logic to lib.rs and expose as public

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::{fs, thread};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 pub fn get_active() -> u64 {
     SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()
@@ -10,6 +11,41 @@ pub fn format_screen_time(active_seconds: u64) -> String {
     let seconds = active_seconds % 60;
 
     format!(
-    "{{\"text\": \"{:02}:{:02}:{:02}\", \"tooltip\": \"Screen Time\" }}",
-    hours, minutes, seconds)
+        "{{\"text\": \"{:02}:{:02}:{:02}\", \"tooltip\": \"Screen Time\" }}",
+        hours, minutes, seconds)
+}
+
+pub fn active_timer(callback: fn(u64)) {
+    let path = "/dev/shm/active_time.text";
+    let mut active_start = match fs::read_to_string(path) {
+        Ok(val) => val.as_str().trim().parse::<u64>().unwrap_or_else(|_| get_active()),
+        Err(_) => {
+            let active = get_active();
+            let _ = fs::write(
+                path,
+                active.to_string()
+            );
+            active
+        }
+    };
+
+    let mut last_active = get_active();
+
+    loop {
+        let active = get_active();
+        if active > last_active + 5 {
+            let eepy_nix = active - last_active - 1;
+            active_start += eepy_nix;
+            let _ = fs::write(path, active_start.to_string());
+        }
+        last_active = active;
+        if active_start > active {
+            active_start = active;
+            let _ = fs::write(path, active_start.to_string());
+        }
+        let active_seconds = active - active_start;
+        callback(active_seconds);
+        thread::sleep(Duration::from_secs(1));
+    }
+
 }
