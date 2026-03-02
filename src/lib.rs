256afa9191bc381a3a27fbd49dd1ad1118dd20f9
shokomergo <shokomergo@gmail.com>
2026-03-02 07:02:52 +0300

    ms tracking

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,26 @@
 use std::{fs, thread};
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
-pub fn get_active() -> u64 {
-    SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()
+pub fn get_active() -> u128 {
+    SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis()
 }
 
-pub fn format_screen_time(active_seconds: u64) -> String {
+pub fn format_screen_time(active_millis: u64) -> String {
+    let active_seconds = active_millis / 1000;
     let hours = active_seconds / 3600;
     let minutes = (active_seconds % 3600) / 60;
     let seconds = active_seconds % 60;
+    let ms = active_millis % 1000;
 
     format!(
-        "{{\"text\": \"{:02}:{:02}:{:02}\", \"tooltip\": \"Screen Time\" }}",
-        hours, minutes, seconds)
+        "{{\"text\": \"{:02}:{:02}:{:02}.{:03}\", \"tooltip\": \"Screen Time\" }}",
+        hours, minutes, seconds, ms)
 }
 
 pub fn active_timer(callback: fn(u64)) {
     let path = "/dev/shm/active_time.text";
     let mut active_start = match fs::read_to_string(path) {
-        Ok(val) => val.as_str().trim().parse::<u64>().unwrap_or_else(|_| get_active()),
+        Ok(val) => val.as_str().trim().parse::<u128>().unwrap_or_else(|_| get_active()),
         Err(_) => {
             let active = get_active();
             let _ = fs::write(
@@ -33,8 +35,8 @@ pub fn active_timer(callback: fn(u64)) {
 
     loop {
         let active = get_active();
-        if active > last_active + 5 {
-            let eepy_nix = active - last_active - 1;
+        if active > last_active + 5000 {
+            let eepy_nix = active - last_active - 1000;
             active_start += eepy_nix;
             let _ = fs::write(path, active_start.to_string());
         }
@@ -43,9 +45,9 @@ pub fn active_timer(callback: fn(u64)) {
             active_start = active;
             let _ = fs::write(path, active_start.to_string());
         }
-        let active_seconds = active - active_start;
-        callback(active_seconds);
-        thread::sleep(Duration::from_secs(1));
+        let active_millis = active - active_start;
+        callback(active_millis.try_into().unwrap());
+        thread::sleep(Duration::from_millis(16));
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use shoko_screen_timer::{active_timer, format_screen_time};
+use std::io::{self, Write};
 
 fn main() {
-    active_timer(|active_seconds| {
-        println!(
-            "{}", format_screen_time(active_seconds)
+    active_timer(|active_millis| {
+        print!(
+            "\r{}", format_screen_time(active_millis)
         );
+        io::stdout().flush().unwrap();
     });
 }
