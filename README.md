55761937fea5853517bc85d8f13b0e7208a4c454
shokomergo <shokomergo@gmail.com>
2026-03-03 13:24:11 +0300

    restore previous version

--- README.md
+++ README.md
@@ -4,8 +4,7 @@ A simple screen timer with json output for status bars (like waybar).
 ## Features
 - Automatically handles system suspend or lid close.
 - Doesn't count sleep time.
-- Uses '/dev/shm' for writing and auto-cleanup on reboot for linux
-- Uses temp directories for windows/macOS and cleaned every new session
+- Uses '/dev/shm' for writing and auto-cleanup on reboot
 - Outputs JSON format for easy GUI integration.
 
 ## Usage
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,6 @@
 use std::{fs, thread};
-use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
-use std::path::{ PathBuf};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
-pub fn get_path() -> PathBuf {
-    if cfg!(target_os = "linux") {
-        let p = "/dev/shm/active_time.text";
-        p.split('/').collect::<PathBuf>()
-    } else {
-        let mut p = std::env::temp_dir();
-        p.push("active_time.text");
-        p
-    }
-}
 pub fn get_active() -> u128 {
     SystemTime::now().duration_since(UNIX_EPOCH).map(|d| d.as_millis()).unwrap_or(0)
 }
@@ -29,26 +18,18 @@ pub fn format_screen_time(active_millis: u64) -> String {
 }
 
 pub fn active_timer(callback: fn(u64)) {
-    let path = get_path();
-    let now_ms = get_active();
-
-    let uptime_ms = Instant::now().elapsed().as_millis();
-    let boot_time_ms = now_ms - uptime_ms;
-    let mut active_start = match fs::read_to_string(&path) {
-        Ok(val) => {
-            let active_saved = val.as_str().trim().parse::<u128>().unwrap_or_else(|_| now_ms);
-            if active_saved < boot_time_ms {
-                let _ = fs::remove_file(&path);
-                now_ms
-            } else {
-                active_saved
-            }
-        },
+    let path = "/dev/shm/active_time.text";
+    let mut active_start = match fs::read_to_string(path) {
+        Ok(val) => val.as_str().trim().parse::<u128>().unwrap_or_else(|_| get_active()),
         Err(_) => {
-               let _ = fs::write(&path, now_ms.to_string());
-                now_ms
-            }
-        };
+            let active = get_active();
+            let _ = fs::write(
+                path,
+                active.to_string()
+            );
+            active
+        }
+    };
 
     let mut last_active = get_active();
 
@@ -57,12 +38,12 @@ pub fn active_timer(callback: fn(u64)) {
         if active > last_active + 5000 {
             let eepy_nix = active - last_active - 1000;
             active_start += eepy_nix;
-            let _ = fs::write(&path, active_start.to_string());
+            let _ = fs::write(path, active_start.to_string());
         }
         last_active = active;
         if active_start > active {
             active_start = active;
-            let _ = fs::write(&path, active_start.to_string());
+            let _ = fs::write(path, active_start.to_string());
         }
         let active_millis = active - active_start;
         callback(active_millis.try_into().unwrap_or(0));
